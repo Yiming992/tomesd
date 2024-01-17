3f775a7623e6323f3fae80788e32460618a56d63
Yiming992 <yimingliuno1@gmail.com>
2024-01-18 05:55:31 +0800

    update

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ trials = 2
 tt = 0
 for _ in range(trials):
     st = time.time()
-    pipeline(prompt="Laundromat Stories: Inside a laundromat on a rainy day. People load clothes into washing machines and read magazines while waiting. Charcoal drawing, chiaroscuro, dramatic
+    pipeline(prompt="Laundromat Stories: Inside a laundromat on a rainy day. People load clothes into washing machines and read magazines while waiting. Charcoal drawing, chiaroscuro, dramatic \
  lighting from overhead fluorescents.", num_inference_steps=20, num_images_per_prompt=batch_size, width = resolution, height=resolution)
     tt += time.time() - st
 print("SDXL no tomesd: avg time", tt/trials)
@@ -22,7 +22,7 @@ pipeline = tomesd.apply_patch(pipeline, ratio=0.75, max_downsample = 4)
 tt = 0
 for _ in range(trials):
     st = time.time()
-    pipeline(prompt="Laundromat Stories: Inside a laundromat on a rainy day. People load clothes into washing machines and read magazines while waiting. Charcoal drawing, chiaroscuro, dramatic
+    pipeline(prompt="Laundromat Stories: Inside a laundromat on a rainy day. People load clothes into washing machines and read magazines while waiting. Charcoal drawing, chiaroscuro, dramatic \
  lighting from overhead fluorescents.", num_inference_steps=20, num_images_per_prompt=batch_size, width = resolution, height=resolution)
     tt += time.time() - st
 print("SDXL w/ tomesd: avg time", tt/trials)
