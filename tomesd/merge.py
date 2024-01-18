d5895efe783562cf7fc75539e13105a5a4f24489
Yiming992 <yimingliuno1@gmail.com>
2024-01-18 18:53:51 +0800

    update

--- tomesd/merge.py
+++ tomesd/merge.py
@@ -63,9 +63,9 @@ def bipartite_soft_matching_random2d(metric: torch.Tensor,
         else:
             idx_buffer = idx_buffer_view
         
-        print(idx_buffer.shape)
         # We set dst tokens to be -1 and src to be 0, so an argsort gives us dst|src indices
-        rand_idx = idx_buffer.reshape(1, -1, 1).argsort(dim=1)
+        #rand_idx = idx_buffer.reshape(1, -1, 1).argsort(dim=1)
+        rand_idx = idx_buffer.reshape(1, -1, 1)
 
         # We're finished with these
         del idx_buffer, idx_buffer_view
